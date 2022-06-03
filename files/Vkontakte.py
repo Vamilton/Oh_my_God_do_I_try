c4690204fe0e0b1208af9356b33cc8d1a51df55d
Vamilton <arteiano@yandex.ru>
2022-06-03 22:45:53 +0300

    It's alive, alive!!!

---
+++ files/Vkontakte.py
@@ -0,0 +1,85 @@
+import configparser
+import requests
+from datetime import datetime
+from PIL import Image
+
+
+class VkPhotos:
+
+
+    def __init__(self):
+        self.path = 'settings.ini'
+        self.config = configparser.ConfigParser()
+        self.config.read(self.path)
+        self.token = self.config.get("Tokens", "vk_token")
+
+
+
+    def _get_user_id(self):
+        flag = True
+        while flag:
+            name = input('Введите id или вот эти буквы вместо него: ')
+            URL = 'https://api.vk.com/method/users.get'
+            params = {
+                'user_ids': name,
+                'access_token': self.token,
+                'v': '5.131'
+            }
+            res = requests.get(URL, params=params)
+            if not bool(res.json()['response']):
+                print('Пользователь не существует')
+                flag = not bool(res.json()['response'])
+            else:
+                if 'can_access_closed' in res.json()['response'][0].keys() and not res.json()['response'][0]['can_access_closed']:
+                    print('Профиль закрыт')
+                elif 'deactivated' in res.json()['response'][0].keys():
+                    print('Профиль неактивен')
+                else:
+                    flag = False
+        user_id = res.json()['response'][0]['id']
+        return user_id
+
+
+
+    def get_size(self, url):
+        foto_url = url
+        raw = requests.get(foto_url, stream=True).raw
+        size = Image.open(raw).size
+        return size
+
+
+    def get_maxsize_photo(self):
+        URL = 'https://api.vk.com/method/photos.get'
+        user_id = self._get_user_id()
+        params = {
+            'user_ids': user_id,
+            'access_token': self.token,
+            'v':'5.131',
+            'owner_id': user_id,
+            'album_id': 'profile',
+            'extended' : '1',
+            'count': '999',
+        }
+        res = requests.get(URL, params=params)
+        photo_dict = {}
+        f_photo_dict = {}
+        photo_list = []
+        like = 0
+        result = res.json()['response']['items']
+        for photos in result:
+            like = photos['likes']['count']
+            date = datetime.fromtimestamp(photos['date']).strftime("%B_%d_%Y_%H-%M-%S")
+            for photo in photos['sizes']:
+                photo['likes'] = like
+                photo['date'] = date
+                if photo['height'] != 0:
+                    photo['size'] = photo['height'] + photo['width']
+                else:
+                    photo['size'] = sum(self.get_size(photo['url']))
+                f_photo = {key: photo[key] for key in photo if key not in ['height', 'width']}
+                photo_dict = {**photo_dict, **f_photo}
+            photo_list.append([*photo_dict.values()])
+            photo_list.sort(key=lambda i: i[4], reverse=True)
+        return photo_list
+
+
---
+++ files/Ya_upload.py
@@ -0,0 +1,57 @@
+import configparser
+import requests
+from progress.bar import IncrementalBar
+import json
+
+
+class YaUploader:
+    def __init__(self):
+        self.path = 'settings.ini'
+        self.config = configparser.ConfigParser()
+        self.config.read(self.path)
+        self.token = self.config.get("Tokens", "ya_token")
+        self.header = {
+            'Content-Type': 'application/json',
+
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def new_folder(self, folder_name):
+        its_url = f'https://cloud-api.yandex.net/v1/disk/resources?path={folder_name}'
+        response = requests.put(its_url, headers=self.header)
+        return response.json()
+
+
+    def ya_upload(self, disk_file_path, photo_url):
+        param = {'url': photo_url, 'overwrite': 'true', 'path': disk_file_path}
+        response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload', headers=self.header, params=param)
+
+    def rec_data(self, f_name, size):
+        rec_data = {}
+        rec_data['file_name'] = f_name
+        rec_data['size'] = size
+        return rec_data
+
+    def rec_info(self, rec_data, folder_name):
+        with open(f'{folder_name}_info.json', 'w') as f:
+            json.dump(rec_data, f)
+
+    def upl_to_ya(self, photo_list, quantity=5):
+        names_list = []
+        response = []
+        folder_name = input('Как назвать папку? ')
+        self.new_folder(folder_name)
+        if len(photo_list) < quantity:
+            quantity = len(photo_list)
+        bar = IncrementalBar('Загрузка фото с ВК на Яндекс', max=quantity)
+        for photo in photo_list[0:quantity]:
+            f_name = (f'{photo[2]}.jpg')
+            if f_name in names_list:
+                f_name = f'{photo[2]}_{photo[4]}.jpg'
+            disk_file_path = f'/{folder_name}/{f_name}'
+            photo_url = photo[0]
+            self.ya_upload(disk_file_path, photo_url)
+            response.append(self.rec_data(f_name, photo[1]))
+            names_list.append(f_name)
+            bar.next()
+        self.rec_info(response, folder_name)
