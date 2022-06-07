f39d7c515f19afda1e8d2c9e36772fec868241b4
Vamilton <arteiano@yandex.ru>
2022-06-07 11:15:58 +0300

    It's alive, alive!!!

--- Yes_I_try.py
+++ Yes_I_try.py
@@ -2,6 +2,8 @@ import configparser
 from files.Vkontakte import VkPhotos
 from files.Ya_upload import YaUploader
 from files.Odnokl import OkPhotos
+from files.Google import GoogleUploader
+import requests
 
 def read_config():
     path = 'settings.ini'
@@ -10,20 +12,26 @@ def read_config():
     return config
 
 def what_to_do():
-    command_from = input('Откуда грузим? (Vk, Ok) ')
+    upl_from = ['вк', 'ок']
+    upl_to = ['яндекс', 'гугл']
+    command_from = input('Откуда грузим? (Вк, Ок) ')
+    while command_from not in upl_from:
+        command_from = input('Я такое не понимаю, введи ещё раз: ')
     command_to = input('Куда грузим? (Яндекс, Гугл) ')
-    if command_from.lower() == 'vk':
+    while command_to not in upl_to:
+        command_to = input('Я такое не понимаю, введи ещё раз: ')
+    if command_from.lower() == 'вк':
         photo_list = VkPhotos().get_maxsize_photo()
-    elif command_from.lower() == 'ok':
+    elif command_from.lower() == 'ок':
         photo_list = OkPhotos().get_album_photos()
     else:
-        print('Прости, я больше ничего не умею')
-
+        print('Прости, я такое не умею')
     if command_to.lower() == 'яндекс':
         rec = YaUploader().upl_to_ya(photo_list)
     elif command_to.lower() == 'гугл':
-        print('Прости, я ещё так не умею')
-
+        rec = GoogleUploader().upl_to_ggl(photo_list)
+    else:
+        print('Прости, я такое не умею')
 
 
 if __name__ == '__main__':
---
+++ files/Google.py
@@ -0,0 +1,59 @@
+import configparser
+import requests
+import json
+from progress.bar import IncrementalBar
+from pprint import pprint
+
+#Токен брать тут: https://developers.google.com/oauthplayground/
+
+class GoogleUploader:
+    def __init__(self):
+        self.path = 'settings.ini'
+        self.config = configparser.ConfigParser()
+        self.config.read(self.path)
+        self.token = self.config.get('Tokens', 'google_token')
+
+
+    def new_ggl_folder(self):
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.token),
+            'Content-Type': 'application/json'
+        }
+        folder_name = input('Как назвать папку для фото? ')
+        url = 'https://www.googleapis.com/drive/v3/files'
+        metadata = {
+            'name': folder_name,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        r = requests.post(url, headers=headers, data=json.dumps(metadata))
+        folder_id = r.json()['id']
+        return folder_id
+
+
+    def goodle_upload(self, folder_id, f_name, f_url):
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.token),
+        }
+        url = 'https://www.googleapis.com/upload/drive/v3/files/?uploadType=multipart'
+        metadata = {
+            'name': f_name,
+            'parents': [folder_id]
+        }
+        files = {
+            'data': ('metadata', json.dumps(metadata), 'application/json; charset=UTF-8'),
+            'file': requests.get(f_url).content
+        }
+        response = requests.post(url, headers=headers, files=files)
+
+    def upl_to_ggl(self, photo_list, quantity=5):
+        names_list = []
+        folder_id = self.new_ggl_folder()
+        if len(photo_list) > quantity:
+            bar = IncrementalBar('Загрузка фото на Гугл', max=quantity)
+            for photo in photo_list[0:quantity]:
+                f_url = photo[0]
+                f_name = (f'{photo[2]}.jpg')
+                if f_name in names_list:
+                    f_name = f'{photo[2]}_{photo[3]}.jpg'
+                self.goodle_upload(folder_id, f_name, f_url)
+                bar.next()
